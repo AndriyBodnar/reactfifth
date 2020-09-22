45ceec3c79cdaeb77516cce16e8516cf750ffc10
Andriy <powerlviv@ukr.net>
2020-09-22 18:52:21 +0300

    Updates

---
+++ precache-manifest.3e7c8b035be817023717429e149526fd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595c60a4ff47342b32c81f0a51480890",
+    "url": "/reactfifth/index.html"
+  },
+  {
+    "revision": "0f838e113f8edd348fe1",
+    "url": "/reactfifth/static/css/main.0602392a.chunk.css"
+  },
+  {
+    "revision": "90672ebed2219b29d8b4",
+    "url": "/reactfifth/static/js/2.84aa5bb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactfifth/static/js/2.84aa5bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f838e113f8edd348fe1",
+    "url": "/reactfifth/static/js/main.892be5f3.chunk.js"
+  },
+  {
+    "revision": "bef0a86142dfbe9e273a",
+    "url": "/reactfifth/static/js/runtime-main.4d474684.js"
+  },
+  {
+    "revision": "c981df5303becacdb08340389060a0d5",
+    "url": "/reactfifth/static/media/anon.c981df53.svg"
+  },
+  {
+    "revision": "371f610ea50e8030a3fa6fffff9ce72a",
+    "url": "/reactfifth/static/media/arrow.371f610e.svg"
+  },
+  {
+    "revision": "8d3e51c7c10bb2d84c72b4682fa605b0",
+    "url": "/reactfifth/static/media/comment.8d3e51c7.svg"
+  },
+  {
+    "revision": "c3ee4a7e57200d73f815f652fac591b3",
+    "url": "/reactfifth/static/media/female.c3ee4a7e.svg"
+  },
+  {
+    "revision": "c2e1643f330325fb8920055d0df3d36e",
+    "url": "/reactfifth/static/media/like.c2e1643f.svg"
+  },
+  {
+    "revision": "2889fe11d673bb8f9e9156efea05eecf",
+    "url": "/reactfifth/static/media/male.2889fe11.svg"
+  },
+  {
+    "revision": "501c6325ded11fd509ea06f807af1107",
+    "url": "/reactfifth/static/media/repost.501c6325.svg"
+  },
+  {
+    "revision": "422b2b5864ed6394e895e26669ea79e7",
+    "url": "/reactfifth/static/media/search.422b2b58.png"
+  },
+  {
+    "revision": "499187f3b67e6683709b237e095b7e31",
+    "url": "/reactfifth/static/media/share.499187f3.svg"
+  },
+  {
+    "revision": "2019cadf6bdc5d95f29a232dff10f6ea",
+    "url": "/reactfifth/static/media/verified.2019cadf.svg"
+  }
+]);
